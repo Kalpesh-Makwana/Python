d8b46d74b5cee414b94af2fdf85124a1628766f6
Kalpesh  Makwana <kalpeshmakwna2610@gmail.com>
2019-12-31 16:09:40 +0530

    Python Practise

---
+++ raindrops.py
@@ -0,0 +1,12 @@
+def convert(number):
+    word=""
+    if((number%3)==0):
+        word+="Pling"
+    if((number%5)==0):
+        word+="Plang"
+    if((number%7)==0):
+        word+="Plong"
+    if(word==""):
+        word+=str(number)
+
+    return word
